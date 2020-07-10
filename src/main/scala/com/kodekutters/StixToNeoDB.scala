5cc64878c924abc32a54b4ff0d1542cd4c79d349
workingdog <ringowathelet@gmail.com>
2020-07-10 18:28:11 +0900

    release 6.0

--- src/main/scala/com/kodekutters/StixToNeoDB.scala
+++ src/main/scala/com/kodekutters/StixToNeoDB.scala
@@ -20,9 +20,9 @@ object StixToNeoDB {
 
   val usage =
     """Usage:
-       java -jar stixtoneodb-6.0.jar -f stix_file db_dir
+       java -jar stixtoneodb-6.0.jar -f hostAddress stix_file db_dir
        or
-       java -jar stixtoneodb-6.0.jar -x stix_file db_dir""".stripMargin
+       java -jar stixtoneodb-6.0.jar -x hostAddress stix_file db_dir""".stripMargin
 
   /**
     * loads a file containing STIX-2 objects, or
@@ -30,20 +30,22 @@ object StixToNeoDB {
     * into a neo4j graph database
     */
   def main(args: Array[String]) {
-    if (args.length < 2)
+    if (args.length < 3)
       println(usage)
     else {
       // to log the progress, comment this line if you don't want any logging at all
       implicit val logger = Logger("StixToNeoDB")
+      // the Neo4j hostname
+      val hostAddress = args(1).trim
       // the input file
-      val infile = new File(args(1))
+      val infile = new File(args(2))
       // the Neo4j db directory
-      val dbDir = if (args.length == 3) args(2).trim else ""
+      val dbDir = if (args.length == 4) args(3).trim else ""
       // if nothing default is to create a new db in
       // the current directory with name stixdb
       val dbFile = if (dbDir.isEmpty) new java.io.File(".").getCanonicalPath + "/stixdb" else dbDir
       // Neo4jLoader and Neo4jFileLoader can take an implicit logger, default NOPLogger
-      val neoLoader = new Neo4jFileLoader(dbFile)
+      val neoLoader = new Neo4jFileLoader(dbFile, hostAddress)
       args(0) match {
         case "-f" =>
           if (infile.getName.endsWith(".zip")) neoLoader.loadBundleZipFile(infile)
